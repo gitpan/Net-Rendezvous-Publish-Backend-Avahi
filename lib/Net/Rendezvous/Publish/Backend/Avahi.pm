43b52d4055db5f47ae674ab858c546823dd9f514
Ioan Rogers <ioanr@cpan.org>
2012-06-27 01:23:53 +0000

    Import of IOANR/Net-Rendezvous-Publish-Backend-Avahi-0.04 from CPAN.
    gitpan-cpan-distribution: Net-Rendezvous-Publish-Backend-Avahi
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         IOANR/Net-Rendezvous-Publish-Backend-Avahi-0.04.tar.gz
    gitpan-cpan-author:       IOANR
    gitpan-cpan-maturity:     released

--- lib/Net/Rendezvous/Publish/Backend/Avahi.pm
+++ lib/Net/Rendezvous/Publish/Backend/Avahi.pm
@@ -1,4 +1,9 @@
 package Net::Rendezvous::Publish::Backend::Avahi;
+{
+  $Net::Rendezvous::Publish::Backend::Avahi::VERSION = '0.04';
+}
+
+# ABSTRACT: Publish zeroconf data via Avahi
 
 # vi: ts=4 sw=4
 
@@ -7,49 +12,57 @@ use warnings;
 
 use Net::DBus;
 
-our $VERSION = 0.03;
-
 sub new {
-	my $class = shift;
-	my $self = {@_};
-	bless $self, $class;
+    my $class = shift;
+    my $self  = {@_};
+    bless $self, $class;
 
-	my $bus = Net::DBus->system;
-	$self->{service} = $bus->get_service('org.freedesktop.Avahi');
-	$self->{server} = $self->{service}->get_object(
-		'/', 'org.freedesktop.Avahi.Server');
+    my $bus = Net::DBus->system;
+    $self->{service} = $bus->get_service('org.freedesktop.Avahi');
+    $self->{server} =
+      $self->{service}->get_object('/', 'org.freedesktop.Avahi.Server');
 
-	return $self;
+    return $self;
 }
 
 sub publish {
-	my $self = shift;
-	my %args = @_;
+    my $self = shift;
+    my %args = @_;
 
-	my $group = $self->{service}->get_object(
-		$self->{server}->EntryGroupNew, 'org.freedesktop.Avahi.EntryGroup');
-	$group->AddService(Net::DBus::dbus_int32(-1), Net::DBus::dbus_int32(-1),
-		Net::DBus::dbus_uint32(0), $args{name}, $args{type},
-		$args{domain}, $args{host}, Net::DBus::dbus_uint16($args{port}),
+    # AddService argument signature is aay.  Split first into key/value
+    # pairs at character \x01 ...
+    my $txt = $args{txt} || [];
+    unless (ref $txt) {
+        $txt = [map { [(split //, $_)] } (split /\x01/, $txt)];
+    }
 
-		# AddService argument signature is aay.  Split first into key/value
-		# pairs at character \x01, then map characters to bytes and add DBus
-		# type.
-		[map {
-			[map {
-				Net::DBus::dbus_byte(ord($_))
-			} (split //, $_)]
-		} (split /\x01/, $args{txt})]);
-	$group->Commit;
+    # ... then map characters to bytes and add DBus type.
+    if (@{$txt}) {
+        foreach my $t (@{$txt}) {
+            map { $_ = Net::DBus::dbus_byte(ord($_)) } @{$t};
+        }
+    }
 
-	return $group;
+    my $group = $self->{service}->get_object($self->{server}->EntryGroupNew,
+        'org.freedesktop.Avahi.EntryGroup');
+    $group->AddService(
+        Net::DBus::dbus_int32(-1), Net::DBus::dbus_int32(-1),
+        Net::DBus::dbus_uint32(0), $args{name},
+        $args{type},               $args{domain},
+        $args{host},               Net::DBus::dbus_uint16($args{port}),
+        $txt
+    );
+
+    $group->Commit;
+
+    return $group;
 }
 
 sub publish_stop {
-	my $self = shift;
-	my ($group) = @_;
+    my $self = shift;
+    my ($group) = @_;
 
-	$group->Free;
+    $group->Free;
 }
 
 sub step {
@@ -57,37 +70,60 @@ sub step {
 
 1;
 
+
 __END__
+=pod
 
 =head1 NAME
 
-Net::Rendezvous::Publish::Backend::Avahi - Publish zeroconf data with the Avahi
-library.
+Net::Rendezvous::Publish::Backend::Avahi - Publish zeroconf data via Avahi
 
-=head1 DESCRIPTION
+=head1 VERSION
 
-This module publishes zeroconf data with the Avahi library.
+version 0.04
 
-It's a backend for the Net::Rendezvous::Publish module.
+=head1 COPYRIGHT AND LICENSE
 
-=head1 PREREQUISITES
+Copyright (c) 2006 by Jack Bates.  All rights reserved.
 
-Net::DBus
+Copyright (c) 2012 by Ioan Rogers.
 
-Net::Rendezvous::Publish
-
-=head1 AUTHOR
-
-Jack Bates <ms419@freezone.co.uk>
-
-=head1 COPYRIGHT
-
-Copyright 2006, Jack Bates.  All rights reserved.
-
-This program is free software.  You can redistribute it and/or modify it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =head1 SEE ALSO
 
-Net::Rendezvous::Publish - The module this module supports.
+=over
 
-L<Avahi|http://avahi.org/>
+=item L<Net::Rendezvous::Publish> - The module this module supports.
+
+=item L<Avahi|http://avahi.org/>
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Ioan Rogers <ioanr@cpan.org>
+
+=item *
+
+Jack Bates <jablko@cpan.org>
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+You can make new bug reports, and view existing ones, through the
+web interface at L<https://github.com/ioanrogers/Net-Rendezvous-Publish-Backend-Avahi/issues>.
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/ioanrogers/Net-Rendezvous-Publish-Backend-Avahi>
+and may be cloned from L<git://github.com/ioanrogers/Net-Rendezvous-Publish-Backend-Avahi.git>
+
+=cut
+
